983883ec6c8e9c27f63b41cb10cba8272e6a0c35
Junsoo Han <hanjs991124@gmail.com>
2023-09-26 01:11:12 +0900

    상하우 움직임 시 안보이는 에러 확인

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -7,63 +7,68 @@ character = load_image('character.png')
 
 
 def handle_events():
-    global running, x, y, move_x, move_y
+    global running, x, y, move_x, move_y, dire, move_frame, idle_frame
 
     # fill here
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         # fill here
         elif event.type == SDL_KEYDOWN:
-            if event.type == SDLK_RIGHT:
+            if event.key == SDLK_RIGHT:
                 move_x += 1
-            elif event.type == SDLK_LEFT:
+                dire = 1
+            elif event.key == SDLK_LEFT:
                 move_x -= 1
-            elif event.type == SDLK_UP:
+                dire = 0
+            elif event.key == SDLK_UP:
                 move_y += 1
-            elif event.type == SDLK_DOWN:
+                dire = 2
+            elif event.key == SDLK_DOWN:
                 move_y -= 1
+                dire = 3
         elif event.type == SDL_KEYUP:
-            if event.type == SDLK_RIGHT:
+            if event.key == SDLK_RIGHT:
                 move_x = 0
-            elif event.type == SDLK_LEFT:
+            elif event.key == SDLK_LEFT:
                 move_x = 0
-            elif event.type == SDLK_UP:
+            elif event.key == SDLK_UP:
                 move_y = 0
-            elif event.type == SDLK_DOWN:
+            elif event.key == SDLK_DOWN:
                 move_y = 0
 
-    x += move_x * 5
-    y += move_y * 5
 
-    if x<0:
-        x=0
-    elif x>TUK_WIDTH:
-        x= TUK_WIDTH-64
+    if x < 0:
+        x = 0
+    elif x > TUK_WIDTH:
+        x = TUK_WIDTH - 64
 
-    if y<0:
-        y=0
-    elif y>TUK_HEIGHT:
-        y= TUK_HEIGHT-64
+    if y < 0:
+        y = 0
+    elif y > TUK_HEIGHT:
+        y = TUK_HEIGHT - 64
 
 
 
 running = True
 x = TUK_WIDTH//2
 y = TUK_HEIGHT//2
-move_x=0
-move_y=0
-frame = 0
-
+move_x = 0
+move_y = 0
+move_frame = 0
+dire = 1
 while running:
     clear_canvas()
-    background.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
-    character.clip_draw(frame * 64, 64, 64, 64, x, y)
+    background.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+    prev_x, prev_y, prev_frame = x, y, move_frame
+    x += move_x * 5
+    y += move_y * 5
+    move_frame = (move_frame + 1) % 10
+    if prev_x != x or prev_y != y or prev_frame != move_frame:
+        character.clip_draw(move_frame * 64, dire * 0, 64, 64, x, y)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 8
 
     delay(0.01)
 close_canvas()
