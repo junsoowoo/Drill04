46d7e3de0a98bed094af08ec02c89e6b052bf4cc
Junsoo Han <hanjs991124@gmail.com>
2023-09-25 22:45:22 +0900

    좌표, 함수 설정

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -1,13 +1,13 @@
 from pico2d import *
 
-
-open_canvas()
-grass = load_image('TUK_GROUD.png')
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+background = load_image('TUK_GROUD.png')
 character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running,
+    global running
 
     # fill here
 
@@ -16,13 +16,32 @@ def handle_events():
         if event.type == SDL_QUIT:
             running = False
         # fill here
+        if event.type == SDL_KEYDOWN:
+            if event.type == SDLK_RIGHT:
+                pass
+            elif event.type == SDLK_LEFT:
+                pass
+            elif event.type == SDLK_UP:
+                pass
+            elif event.type == SDLK_DOWN:
+                pass
+
 
 
 running = True
-x = 800 // 2
+x = TUK_WIDTH//2
+y = TUK_HEIGHT//2
+move_x=0
+move_y=0
 frame = 0
 
-# fill here
+while running:
+    clear_canvas()
+    background.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 8
 
 
 close_canvas()
