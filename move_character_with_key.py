9be8325d202eb5656a3e2b0a5908b5af9562a2c6
Junsoo Han <hanjs991124@gmail.com>
2023-09-27 20:18:59 +0900

    프로그램 종료추가,

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -3,18 +3,16 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 background = load_image('TUK_GROUND.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
 
 def handle_events():
     global running, x, y, move_x, move_y, dire, move_frame, idle_frame
 
-    # fill here
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 move_x += 1
@@ -37,7 +35,8 @@ def handle_events():
                 move_y = 0
             elif event.key == SDLK_DOWN:
                 move_y = 0
-
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
     if x < 0:
         x = 0
@@ -49,25 +48,21 @@ def handle_events():
     elif y > TUK_HEIGHT:
         y = TUK_HEIGHT - 64
 
-
-
 running = True
 x = TUK_WIDTH//2
 y = TUK_HEIGHT//2
-move_x = 0
-move_y = 0
-move_frame = 0
+move_x, move_y ,move_frame= 0, 0, 0
 dire = 1
+
 while running:
     clear_canvas()
     background.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
     x += move_x * 5
     y += move_y * 5
-    move_frame = (move_frame + 1) % 10
-    character.clip_draw(move_frame * 64, dire * 0, 64, 64, x, y)
+    character.clip_draw(move_frame * 100, dire * 0, 100, 100, x, y)
     update_canvas()
+    move_frame = (move_frame + 1) % 10
     handle_events()
 
-    delay(0.01)
 close_canvas()
 
